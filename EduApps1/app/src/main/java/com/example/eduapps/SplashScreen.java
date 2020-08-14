f565ac44abf11668b55e8c8d6985d3446b3c2938
Juan P. Isaza <jisazamp@gmail.com>
2020-08-13 22:36:42 -0500

    Proyecto base

---
+++ EduApps1/app/src/main/java/com/example/eduapps/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.example.eduapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int SPLASH_SCREEN =5000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+    }
+}
