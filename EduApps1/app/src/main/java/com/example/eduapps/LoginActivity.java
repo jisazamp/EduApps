e61dc508ac39ef81f95c40f616ca00b01be1c05e
Juan P. Isaza <jisazamp@gmail.com>
2020-08-13 23:23:05 -0500

    Navegación de login y registro

--- EduApps1/app/src/main/java/com/example/eduapps/LoginActivity.java
+++ EduApps1/app/src/main/java/com/example/eduapps/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.eduapps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btn_iniciar_sesion = (Button)findViewById(R.id.iniciarSesionButton);
+        btn_iniciar_sesion.setOnClickListener(new View.OnClickListener() {
+
+            // metodo abstracto que tengo que implementar
+            @Override
+            public void onClick(View view) {
+                Intent intent;
+                intent = new Intent(LoginActivity.this, TeacherHomeActivity.class);
+                intent.putExtra("valor", "hola");
+                startActivity(intent);
+            }
+        });
     }
 }
--- EduApps1/app/src/main/java/com/example/eduapps/RegisterActivity.java
+++ EduApps1/app/src/main/java/com/example/eduapps/RegisterActivity.java
@@ -2,7 +2,10 @@ package com.example.eduapps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button btn_registrar = (Button)findViewById(R.id.registrarButton);
+        btn_registrar.setOnClickListener(new View.OnClickListener() {
+
+            // metodo abstracto que tengo que implementar
+            @Override
+            public void onClick(View view) {
+                Intent intent;
+                intent = new Intent(RegisterActivity.this, TeacherHomeActivity.class);
+                intent.putExtra("valor", "hola");
+                startActivity(intent);
+            }
+        });
     }
 }
