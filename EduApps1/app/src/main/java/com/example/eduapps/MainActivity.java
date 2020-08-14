eacfae62d6f93ef0e502488ab3817e7121d6f83b
Juan P. Isaza <jisazamp@gmail.com>
2020-08-14 00:27:37 -0500

    Listener unico

--- EduApps1/app/src/main/java/com/example/eduapps/MainActivity.java
+++ EduApps1/app/src/main/java/com/example/eduapps/MainActivity.java
@@ -7,14 +7,40 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public void procesarEvento(int opc) {
+
+        Intent intent;
+
+        switch (opc)
+        {
+            case R.id.loginButton:
+                intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.registroButton:
+                intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // declaracion de botones
         Button btn_login = (Button)findViewById(R.id.loginButton);
+        Button btn_registro = (Button)findViewById(R.id.registroButton);
+
+        // set del listener a los botones
+        btn_login.setOnClickListener(this);
+        btn_registro.setOnClickListener(this);
+
+        /*
         btn_login.setOnClickListener(new View.OnClickListener() {
 
             // metodo abstracto que tengo que implementar
@@ -27,7 +53,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button btn_registro = (Button)findViewById(R.id.registroButton);
         btn_registro.setOnClickListener(new View.OnClickListener() {
 
             // metodo abstracto que tengo que implementar
@@ -38,6 +63,11 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("valor", "hola");
                 startActivity(intent);
             }
-        });
+        }); */
+    }
+
+    @Override
+    public void onClick(View view) {
+        procesarEvento(view.getId());
     }
 }
