4079b74490a44ef680e95ed936bd060bd6a037ba
Juan P. Isaza <jisazamp@gmail.com>
2020-08-18 23:11:53 -0500

    Cambios a los Layout

--- EduApps1/app/src/main/java/com/example/eduapps/SplashActivity.java
+++ EduApps1/app/src/main/java/com/example/eduapps/SplashActivity.java
@@ -5,17 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class SplashScreen extends AppCompatActivity {
-
+public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        int SPLASH_SCREEN =5000;
+        setContentView(R.layout.activity_splash);
+
+        int SPLASH_SCREEN =3000;
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
                 startActivity(intent);
                 finish();
             }
