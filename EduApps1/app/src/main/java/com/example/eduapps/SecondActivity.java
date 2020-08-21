551c7a695b04cf7e8688ee7e2d93ced02e8adb0e
Juan P. Isaza <jisazamp@gmail.com>
2020-08-21 08:12:25 -0500

    Corrección SecondActivity

--- EduApps1/app/src/main/java/com/example/eduapps/SecondActivity.java
+++ EduApps1/app/src/main/java/com/example/eduapps/SecondActivity.java
@@ -14,18 +14,23 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        Button btn = (Button)findViewById(R.id.agregarButton);
-        btn.setOnClickListener(new View.OnClickListener() {
+        /*
+         * LAS LÍNEAS COMENTADAS ABAJO SON LAS QUE
+         * ME ESTÁN CAUSANDO PROBLEMAS
+         */
 
-            // metodo abstracto que tengo que implementar
-            @Override
-            public void onClick(View view) {
-                Intent intent;
-                intent = new Intent(SecondActivity.this, TeacherHomeActivity.class);
-                intent.putExtra("valor", "hola");
-                startActivity(intent);
-                finish();
-            }
-        });
+        Button btn = (Button)findViewById(R.id.agregarButton);
+//        btn.setOnClickListener(new View.OnClickListener() {
+//
+//            // metodo abstracto que tengo que implementar
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent;
+//                intent = new Intent(SecondActivity.this, TeacherHomeActivity.class);
+//                intent.putExtra("valor", "hola");
+//                startActivity(intent);
+//                finish();
+//            }
+//        });
     }
 }
