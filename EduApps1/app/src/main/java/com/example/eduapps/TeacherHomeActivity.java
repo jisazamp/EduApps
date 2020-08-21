71d8d3fa9807f55a969815ab76adb0b24443350d
Juan P. Isaza <jisazamp@gmail.com>
2020-08-20 23:27:58 -0500

    RecyclerView a la vista de TeacherHome

--- EduApps1/app/src/main/java/com/example/eduapps/TeacherHomeActivity.java
+++ EduApps1/app/src/main/java/com/example/eduapps/TeacherHomeActivity.java
@@ -1,6 +1,8 @@
 package com.example.eduapps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,11 +11,24 @@ import android.widget.Button;
 
 public class TeacherHomeActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    String s1[], s2[];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_home);
 
+        recyclerView = findViewById(R.id.recyclerView);
+
+        s1 = getResources().getStringArray(R.array.sesiones);
+        s2 = getResources().getStringArray(R.array.descripcion);
+
+        MyAdapter myAdapter = new MyAdapter(this, s1, s2);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // intent para el boton de registrar sesion
         Button btn_registrar_sesion = (Button)findViewById(R.id.agregarSesionButton);
         btn_registrar_sesion.setOnClickListener(new View.OnClickListener() {
 
